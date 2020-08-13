481cd9cab4cda3bf7d7228a4318076d3ab52642f
Sanny Do <sanny.do94@gmail.com>
2020-08-13 05:03:29 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,7 +1,6 @@
-countdown = 10
 count = 10
 
-while count < 10 && count != countdown do
+while count >= 1 && count != countdown do
   break if count == countdown
   puts "Happy New Year!"
   count = count + 1
